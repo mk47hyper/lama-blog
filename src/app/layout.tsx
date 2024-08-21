c8f2ee40398df966d6bcc057c2d8814b1600c8ee
m.kirilov <m.kirilov@reply.com>
2024-08-21 10:41:11 +0100

    01- Adding navbar and footer.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
-          <div className="wrapper">{children}</div>
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
         </div>
       </body>
     </html>
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return <div className={styles.container}>Footer</div>;
+};
+
+export default Footer;
